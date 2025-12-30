1515f25eb71d8cd5324696cdc931b493fe3de272
habtamubaye-devsec <habtamubaye2127@gmail.com>
2025-12-30 09:05:49 +0300

    refactor: clean up AppModule by removing unused imports and modules; update greeting in AppService

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,37 +1,23 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { ApolloDriver } from '@nestjs/apollo';
-import { GraphQLModule } from '@nestjs/graphql';
-import { AppController } from './app.controller';
-import { AppResolver } from './app.resolver';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
 import { UserModule } from './user/user.module';
-import { SupabaseModule } from './supabase/supabase.module';
-import { UploadsModule } from './uploads/uploads.module';
-import { join } from 'path';
 
 @Module({
   imports: [
+    // Load .env globally
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    GraphQLModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      driver: ApolloDriver,
-      useFactory: async (configService: ConfigService) => ({
-        playground: true,
-        autoSchemaFile:  join(process.cwd(), 'src/schema.gql'),
-        sortSchema: true,
-      }),
-    }),
-    SupabaseModule,
-    UploadsModule,
+
+    // Authentication module
     AuthModule,
+
     UserModule,
   ],
   controllers: [AppController],
-  providers: [AppService, AppResolver],
+  providers: [AppService],
 })
 export class AppModule {}
