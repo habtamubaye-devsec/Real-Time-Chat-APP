5180d378014d5f3583ae0a97f684745646f92b7c
habtamubaye-devsec <habtamubaye2127@gmail.com>
2026-01-01 03:11:43 +0300

    feat: add GraphQL code generation configuration and update main entry point for routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,10 @@
-import React, { Children } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { ApolloProvider } from "@apollo/client/react";
 import { client } from "./apolloClient";
 import { MantineProvider } from "@mantine/core";
 
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 
 // If you use Mantine global styles in your project, keep this import too:
@@ -13,14 +12,14 @@ import Home from "./pages/Home";
 
 
 const router = createBrowserRouter([
-  { path:"/", children: <Home />  },
+  { path: "/", element: <Home /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <MantineProvider>
-        <App />
+          <RouterProvider router={router} />
       </MantineProvider>
     </ApolloProvider>
   </React.StrictMode>
